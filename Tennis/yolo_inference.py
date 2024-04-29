2a96059e1cf4e49ed537fe4bd762ccc74fcf0876
Matty-7 <jh730@duke.edu>
2024-04-29 21:49:38 +0800

    court_keypoints

--- Tennis/yolo_inference.py
+++ Tennis/yolo_inference.py
@@ -1,8 +1,12 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x')
+# model = YOLO('Tennis/models/yolov8x.pt')
+model = YOLO('Tennis/models/yolo5_last.pt')
 
-result = model.predict('Tennis/input_videos/image.png', save=True)
+# result = model.predict('Tennis/input_videos/image.png', save=True)
+# result = model.predict('Tennis/input_videos/input_video.mp4', save=True)
+
+result = model.predict('Tennis/input_videos/input_video.mp4', conf=0.2, save=True)
 
 print(result)
 
