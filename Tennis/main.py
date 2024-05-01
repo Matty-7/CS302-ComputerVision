c7f0537d8a88aec9e8103f9b6143b071e3aa441b
Matty-7 <jh730@duke.edu>
2024-05-02 00:29:20 +0800

    Detect both balls and players

--- Tennis/main.py
+++ Tennis/main.py
@@ -1,5 +1,5 @@
 from utils import read_video, save_video
-from trackers import PlayerTracker
+from trackers import PlayerTracker, BallTracker
 
 def main():
     # Read Video
@@ -8,17 +8,22 @@ def main():
 
     # Detect Players
     player_tracker = PlayerTracker(model_path="/Users/huanjingheng/CS302-ComputerVision/Tennis/models/yolov8x.pt")
+    ball_tracker = BallTracker(model_path="/Users/huanjingheng/CS302-ComputerVision/Tennis/models/yolo5_last.pt")
+
     player_detections = player_tracker.detect_frames(video_frames,
                                                      read_from_stub = True,
                                                      stub_path = "tracker_stubs/player_detections.pkl")
-
+    ball_detections = ball_tracker.detect_frames(video_frames,
+                                                     read_from_stub = True,
+                                                     stub_path="tracker_stubs/ball_detections.pkl"
+                                                     )
     # Draw output
 
     # Draw Player Bounding Boxes
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
+    output_video_frames = ball_tracker.draw_bboxes(output_video_frames, ball_detections)
 
-
-    save_video(output_video_frames, "output_videos/output_video.avi")
+    save_video(output_video_frames, "output_videos/output_video3.avi")
 
 if __name__ == "__main__":
     main() 
